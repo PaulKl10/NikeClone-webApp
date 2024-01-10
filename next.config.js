d5cdeeef9bca8bc8c7c6e6d1fa7d1ea738f6cbf8
Alaaeddine Gharbi <gharbi.alaaeddine@gmail.com>
2024-01-10 14:42:25 +0100

    initial commit by Alaaeddine

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+
+  // i18n: {
+  //   locales: ['en', 'fr'],
+  //   defaultLocale: 'fr',
+  // },
+  
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+}
