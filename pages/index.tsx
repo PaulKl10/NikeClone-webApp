a8046eabc32ceb0e5fa4f6692d9283435eb632f5
Alaaeddine Gharbi <gharbi.alaeddine@gmail.com>
2024-02-06 10:39:04 +0100

    exo variant commit by Alaaeddine

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import { sanityClient } from '@/sanity'
 
 interface Props{
   products: any
+  variants: any
 }
 
-export default function Home({products}:Props) {
-  console.log(products)
+export default function Home({products, variants}:Props) {
+  console.log('Our Products:', products)
+  console.log('Our Variants:', variants)
   return (
     <HomePageView/>
   )
@@ -16,14 +18,52 @@ export default function Home({products}:Props) {
 export async function getServerSideProps() {
   //Fetch Product query
   const productQuery = `
-  *[_type == "product"]`
+  *[_type == "product"]{
+    _id,
+    _createdAt,
+    name,
+    slug,
+    mainImage,
+    price,
+    description,
+    category->{
+      name
+    }
+  }`;
 
   // Fetch data from external API
   const products = await sanityClient.fetch(productQuery)
+
+  //Fetch Product query
+  const variantQuery = `
+  *[_type == "variant"]{
+    product->
+    {
+      _id,
+      _createdAt,
+      name,
+      slug,
+      mainImage,
+      price,
+      description,
+      category->{
+        name
+      }
+    },
+    color->{
+      value
+    },
+    mainImage,
+    images
+  }`;
+
+  // Fetch data from external API
+  const variants = await sanityClient.fetch(variantQuery)
   
   // Pass data to the page via props
   return { props: 
     { 
-      products 
+      products,
+      variants
     } }
 }
